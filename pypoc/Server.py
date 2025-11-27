ab509253e90a358acbe871384477db63aa07f6d4
Dimitri <dimitri.ray.boyce@gmail.com>
2025-11-27 17:55:41 -0500

    make parsing function

--- pypoc/Server.py
+++ pypoc/Server.py
@@ -3,8 +3,25 @@ sep = "\u2063"
 
 def packet(type: str, username: str, port: int, message: str):
     if type == "M":
-        return(type+sep+username+sep+message)
+        return(type+sep+username+sep+message+sep)
     if type == "J":
-        return(type+sep+username+sep+port)
+        return(type+sep+username+sep+str(port)+sep)
 
-print(packet("M", "dimit", 3978, "hello world"))
+def parsePacket(packetContents: str):
+    temptracker = 0
+    temp = ""
+    returnlist = ["","",""]
+    for i in packetContents:
+        if i != sep:
+            temp = temp + i
+        else:
+            returnlist[temptracker] = temp
+            temp = ""
+            temptracker += 1
+    return(returnlist)
+
+
+pacdata = packet("J", "dimit", 3978, "hello world")
+print(pacdata)
+print(parsePacket(pacdata))
+
